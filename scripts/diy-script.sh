d1969d3b69dd99292e161cd9ab52cb745029b40a
losky <271923379@qq.com>
2025-04-02 21:48:05 +0800

    Update diy-script.sh

--- scripts/diy-script.sh
+++ scripts/diy-script.sh
@@ -1,6 +1,6 @@
 # 修改默认IP & 固件名称
-sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
-sed -i "s/hostname='.*'/hostname='Ooya'/g" package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
+sed -i "s/hostname='.*'/hostname='losky'/g" package/base-files/files/bin/config_generate
 
 # TTYD 免登录
 sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
@@ -8,8 +8,13 @@ sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.
 # 移除要替换的包
 rm -rf feeds/packages/net/mosdns
 rm -rf feeds/packages/net/msd_lite
-rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/packages/net/smartdns
 rm -rf feeds/packages/net/adguardhome
+rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf feeds/luci/themes/luci-theme-netgear
+rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/luci/applications/luci-app-netdata
+rm -rf feeds/luci/applications/luci-app-serverchan
 
 # Git稀疏克隆，只克隆指定目录到本地
 function git_sparse_clone() {
@@ -34,6 +39,10 @@ git clone --depth=1 https://github.com/ximiTech/msd_lite package/msd_lite
 
 # MosDNS
 git clone --depth=1 https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
+
+
+git clone --depth=1 https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot
+git clone --depth=1 https://github.com/sirpdboy/luci-app-partexp.git package/luci-app-partexp
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
